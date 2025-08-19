c50f6e4dd9e8ef714095916083940fcb1fdd974a
YunTaeSeong <xoqkf1234@naver.com>
2025-08-19 10:00:11 +0900

    멀티모듈 설정

---
+++ consumer/src/main/java/com/notification/kafka/KafkaNotificationConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.notification.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNotificationConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaNotificationConsumerApplication.class, args);
+	}
+
+}
